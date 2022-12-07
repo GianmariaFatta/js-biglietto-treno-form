996debd38979ea5128a30054d28dcde97773ce6f
GianmariaFatta <gianmaria.fatta@gmail.com>
2022-12-07 02:53:56 +0100

    style none commit

--- js/script.js
+++ js/script.js
@@ -14,37 +14,108 @@
 // Solo una volta che il milestone 1 sarà completo e funzionante allora realizzeremo un form in pagina in cui l’utente potrà inserire i dati e visualizzare il calcolo finale con il prezzo.
 // Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagina (il prezzo dovrà essere formattato con massimo due decimali, per indicare i centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-//recupero elemento in pagina  
-const moneyElement = document.getElementById("money");
-
-//chiederre l'età all'utente    
-const userAge = parseInt(prompt("Quanti anni hai?" , 28).trim());
-console.log(userAge);
-
-//chiedere all'utente quanti km deve percorrere 
-const userDistance = parseInt(prompt("Quanti km devi percorrere?" , 50).trim());
-console.log(userDistance);
 
 
-//calcola il prezzo del biglietto
-const userPrice = (0.21 * userDistance);
-console.log(userPrice);
+// //recupero elementi in pagina 
+const userName = document.getElementById("username");
+const nameElement = document.getElementById("name")
+const genButton =document.getElementById("generate");
+const eraseButton =document.getElementById("erase")
+const moneyElement =document.getElementById("money")
+const kmsElement= document.getElementById("kms")
+const ageElement =document.getElementById("age")
+const discountElement=document.getElementById("discount")
+const trainElement=document.getElementById("train")
+const codElement=document.getElementById("code")
 
-//stabilire sconto del 20%
-let discountSmall = ((userPrice * 20) / 100 )
-console.log(discountSmall);
 
-//stabilire sconto del 40%
-let discountBig = ((userPrice * 40) / 100)
-console.log(discountBig);
- 
 
-//se è under 18...
-if (isNaN (userAge) || isNaN (userDistance) || (userAge <=0) || (userDistance <=0)) {
-    document.getElementById("money").innerText = "HAI INSERITO CARATTERI INVALIDI. RIPROVA";
- }
-else if (userAge < 18){
-    moneyElement.innerText = "il prezzo del tuo biglietto è di €" + (userPrice - discountSmall).toFixed(2)
-}else if (userAge >= 65){
-    moneyElement.innerText = "il prezzo del tuo biglietto è di €" + (userPrice - discountBig).toFixed(2) }
-else { moneyElement.innerText = "il prezzo del tuo biglietto è di €" + userPrice.toFixed(2)};
+
+
+//aggancia il bottone
+eraseButton.addEventListener("click", function(){
+    nameElement.value="";
+    kmsElement.value="";
+    userName.innerText = " ";
+    moneyElement.innerText="";
+
+    
+})
+genButton.addEventListener("click", function(){
+
+
+    //numero carrozza
+    
+
+    let randomNumber =Math.floor(Math.random()*(10)+1);
+    console.log(randomNumber)
+    trainElement.innerText= randomNumber;
+
+    //codice cp
+
+    let randomNumberCode =Math.floor(Math.random()*(99999)+10000);
+    console.log(randomNumberCode)
+    codElement.innerText= randomNumberCode;
+    
+    
+    //gestimao il nome
+    const text = nameElement.value.trim();
+    console.log(text);
+    userName.innerText = text;
+    nameElement.value="";
+    
+    
+    
+    
+    // gestiamo il prezzo
+    const distance = kmsElement.value.trim();
+    const discountBig= (distance * 0.21)*0.6;
+    const discountSmall =(distance *0.21)*0.8
+
+    
+    if(isNaN (ageElement.value) || isNaN (kmsElement.value) || (ageElement.value <=0) || (kmsElement.value <=0)){
+        moneyElement.innerText= "HAI INSERITO CARATTERI INVALIDI, RIPROVA";
+        userName.innerText =""
+    }    
+    
+    
+    else if(ageElement.value >= 65 ){
+        moneyElement.innerText= "€" + (discountBig).toFixed(2);
+        ageElement.value="";
+        kmsElement.value="";
+        discountElement.innerText="Sconto del 40% over 65";
+        
+    }else if (ageElement.value < 18 ){
+        moneyElement.innerText= "€" + (discountSmall).toFixed(2);
+        ageElement.value="";
+        kmsElement.value="";
+        discountElement.innerText="Sconto del 20% under 18"
+    }
+    else{
+        
+        //costo di base
+        const distance = kmsElement.value.trim();
+        moneyElement.innerText= "€" + (distance * 0.21).toFixed(2);
+        ageElement.value="";
+        kmsElement.value="";
+        discountElement.innerText="///"
+    }
+    
+
+    
+});
+
+
+
+
+// //calcola il prezzo del biglietto
+
+//stabilirire la distanza che l'utente vuole percorrere
+
+
+// //stabilire sconto del 20%
+// //stabilire sconto del 40%
+
+
+//aggancio l'event listenear al button
+
